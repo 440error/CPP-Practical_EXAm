13f12dcec3b822ab2095b3df1c15ada54e56015f
Prakhar <rinayush@gmail.com>
2022-03-11 11:07:06 +0530

    added cout statements

--- 3.cpp
+++ 3.cpp
@@ -16,27 +16,31 @@ int main()
     ofstream f2;
     f2.open("q_13output.txt");
 
-    cout << "Start entering values ............." << endl;
-    for (int i = 0; i <=5; i++)
+    int n;
+    cout << "how many records u want to enter: ";
+    cin >> n;
+
+
+    for (int i = 1; i <= n; i++)
     {
         Student obj;
 
-        cout << "Enter the Roll No of student no-" << i << endl;
+        cout << "Enter the Roll No of student no " << i <<": ";
         cin >> obj.RollNo;
         f2 << obj.RollNo << "  ";
 
-        cout << "Enter the Name of student no-" << i << endl;
+        cout << "Enter the Name of student no " << i <<": ";
         cin.ignore();
         getline(cin, obj.Name);
         f2 << obj.Name << "    ";
 
-        cout << "Enter the Class of student no-" << i << endl;
+        cout << "Enter the Class of student no " << i <<": ";
         cin >> obj.Class;
 
-        cout << "Enter the Year of student no-" << i << endl;
+        cout << "Enter the Year of student no " << i <<": ";
         cin >> obj.Year;
 
-        cout << "Enter the Total Marks obtained by student no-" << i << endl;
+        cout << "Enter the Total Marks obtained by student no " << i << ": ";
         cin >> obj.TotalMarks;
 
         f2 << obj.Class << "  " << obj.Year << "  " << obj.TotalMarks << endl;
